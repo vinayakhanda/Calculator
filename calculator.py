7949af249c2b4a308473bef448e6ed7d33683331
vinayakhanda <86475762+vinayakhanda@users.noreply.github.com>
2021-08-21 18:56:58 +0530

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,15 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+print("Answer is")print(num1 + num2)
+print("This calculator is made by Vinayak Handa")
+print(" 20 april 2021")
+
+
+
+
+
+
+
+
+
+
